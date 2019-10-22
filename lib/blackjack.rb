b197c06a679bd2a114ee87715b20f80bef07bfc2
Buford Eeds <bufordeeds8@gmail.com>
2019-10-22 16:51:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
 rand(1..11)
 end
 
-def display_card_total(total)
-puts "Your cards add up to #{total}"
+def display_card_total(card_total)
+puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -18,7 +18,7 @@ def get_user_input
 gets.chomp
 end
 
-def end_game
+def end_game(card_total)
  "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
