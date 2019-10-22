6348d9606ebddcb363fbac97674c5e640fd7ee8f
Buford Eeds <bufordeeds8@gmail.com>
2019-10-22 16:51:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,7 +19,7 @@ gets.chomp
 end
 
 def end_game(card_total)
- "Sorry, you hit #{card_total}. Thanks for playing!"
+ puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
